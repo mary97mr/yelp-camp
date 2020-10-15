d05de30712248c1831df7388427079f523843b00
Mari <marymendirojas@gmail.com>
2020-10-15 12:09:41 +0200

    Added user profile

--- models/user.js
+++ models/user.js
@@ -4,6 +4,10 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const userSchema = new mongoose.Schema({
     username: String,
     password: String,
+    avatar: String,
+    firstName: String,
+    lastName: String,
+    email: String,
     isAdmin: {type: Boolean, default: false}
 });
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const passport = require("passport");
 const User = require("../models/user");
+const Campground = require("../models/campground");
 
 // Root Route
 router.get("/", function(req, res) {
@@ -20,7 +21,13 @@ router.get("/register", function(req, res) {
 
 // handdle sign up logic
 router.post("/register", function(req, res) {
-    var newUser = new User({username: req.body.username});
+    var newUser = new User({
+        username: req.body.username,
+        avatar: req.body.avatar,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email
+    });
     if(req.body.adminCode === "secretcode123") {
         newUser.isAdmin = true;
     }
@@ -60,6 +67,26 @@ router.get("/logout", function(req, res) {
     req.logout();
     req.flash("success", "You logged out")
     res.redirect("/campgrounds");
+});
+
+// ===================
+// USER PROFILE ROUTES
+// ===================
+
+router.get("/users/:id", function(req, res) {
+    User.findById(req.params.id, function(err, foundUser) {
+        if(err) {
+            req.flash("error", "Something went wrong.");
+            res.redirect("back");
+        }
+        Campground.find().where("author.id").equals(foundUser._id).exec(function(err, campgrounds) {
+            if(err) {
+                req.flash("error", "Something went wrong.");
+                res.redirect("back");
+            }
+            res.render("users/show", {user: foundUser, campgrounds:campgrounds});
+        })
+    });
 });
 
 module.exports = router;
