5b4dd68d5d1b7cfe9b5c97add2659c8a93b9d516
Mari <marymendirojas@gmail.com>
2021-02-22 22:06:02 +0100

    deploying to heroku config

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var methodOverride = require("method-override");
 app.use(methodOverride("_method"));
 
 var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/yelp_camp', {
+var dbUrl = process.env.DB_URL || 'mongodb://localhost/yelp_camp';
+var secret = process.env.SECRET || "thisshouldbeabettersecret";
+mongoose.connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
@@ -31,7 +33,7 @@ var flash         = require("connect-flash");
 var Campground   = require("./models/campground");
 var Comment      = require("./models/comment");
 var User         = require("./models/user");
-var Notification = require("./models/user");
+var Notification = require("./models/notification");
 
 // Requiring All Routes
 var campgroundRoutes = require("./routes/campgrounds");
@@ -40,13 +42,33 @@ var reviewRoutes     = require("./routes/reviews");
 var indexRoutes      = require("./routes/index");
 var seedDB           = require("./seeds");
 
-// PASSPORT CONFIGURATION
-app.use(require("express-session")({
-    secret: "My cat is cute",
-    resave: false,
-    saveUninitialized:false
-}));
+var session = require("express-session");
+var MongoStore = require("connect-mongo")(session);
 
+var store = new MongoStore({
+    url: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
+// SESSION CONFIG
+var sessionConfig = {
+    store,
+    name: "session",
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
+
+// PASSPORT CONFIGURATION
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
@@ -81,7 +103,8 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
 app.use(indexRoutes);
 
+var port = process.env.PORT || 3000;
 // Listen Port Route
-app.listen(3000, function() {
-    console.log("YelpCamp server running on port 3000");
+app.listen(port, function() {
+    console.log(`YelpCamp server running on port ${port}`);
 });
